c7213aa35afdbafd362f68493996f9f2ca1136e2
Tung <khuatthanhtung09@gmail.com>
2019-01-17 18:03:19 +0200

    Updates

---
+++ precache-manifest.e03e6f603263ec8fa448d7d83082105e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcc355a54670d8213d58",
+    "url": "/tk-clock-and-weather-utilities/static/css/main.647d3c13.chunk.css"
+  },
+  {
+    "revision": "dcc355a54670d8213d58",
+    "url": "/tk-clock-and-weather-utilities/static/js/main.dcc355a5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/tk-clock-and-weather-utilities/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "090d62cff16e03854fb1",
+    "url": "/tk-clock-and-weather-utilities/static/js/runtime~main.090d62cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tk-clock-and-weather-utilities/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "06b9ed2ffeb5dcf974e612f3cf84b042",
+    "url": "/tk-clock-and-weather-utilities/index.html"
+  }
+];
